50a001d6aaa93ead31cc2643709536cb42a346a6
JENkt4k <james.e.nelson@gmail.com>
2024-04-05 15:14:01 -0500

    wrong directory

--- install_deps.py
+++ install_deps.py
@@ -4,7 +4,7 @@ import os
 
 #path to python.exe
 python_exe = os.path.join(sys.prefix, 'bin', 'python')
-py_lib = os.path.join(sys.prefix, 'lib', 'site-packages','pip')
+py_lib = os.path.join(sys.prefix, 'bin','pip')
 
 #install opencv
 subprocess.call([python_exe, py_lib, "install", "opencv_python"])

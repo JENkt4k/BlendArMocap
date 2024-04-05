674bd85fac4866ea7a9a3740e5f8eddeda23ac5f
JENkt4k <james.e.nelson@gmail.com>
2024-04-05 15:08:51 -0500

    omg

--- install_deps.py
+++ install_deps.py
@@ -3,7 +3,7 @@ import sys
 import os
 
 #path to python.exe
-python_exe = os.path.join(sys.prefix, 'bin', 'python.exe')
+python_exe = os.path.join(sys.prefix, 'bin', 'python')
 py_lib = os.path.join(sys.prefix, 'lib', 'site-packages','pip')
 
 #install opencv
